ad4981e35efeeb7ef6828d337b005aeec25fe204
Ohad Aloni <ohadaloni@Ohads-MacBook-Pro.local>
2025-04-05 14:39:18 +0300

    main

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import {  ThemeProvider } from "@mui/material/styles";
 import theme from "./themes";
-
+import './main.css';
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
